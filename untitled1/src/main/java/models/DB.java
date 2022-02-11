8e6f36096bf9e7e4dbd868fa33a319a631eea12a
edwardmbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-02-11 18:34:23 +0300

    Update DB.java

--- untitled1/src/main/java/models/DB.java
+++ untitled1/src/main/java/models/DB.java
@@ -4,7 +4,7 @@ import org.sql2o.*;
 
 public class DB {
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-158-232-223.compute-1.amazonaws.com:5432/d74l98ruq4hcnn?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "zhrozwhrlajtdf", "a69b2dd3de2c30fb05fce08b5ac98bbef3bc218f66740511b071dea25960aeae");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-158-232-223.compute-1.amazonaws.com:5432/d74l98ruq4hcnn?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "zhrozwhrlatdf", "a69b2dd3de2c30fb05fce08b5ac98bbef3bc218f66740511b071dea25960aeae");
 
 
 //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/untitled1", "", "");
